6a7f79432722ba3bb80d26c727cd35066ce3dbd7
Ivan <Ivan7771998>
2020-03-16 21:37:41 +0300

    first commit

---
+++ www/include/block-header.php
@@ -0,0 +1,27 @@
+<div id="block-header">
+<div id="header-top-block">
+
+<ul id="header-top-menu">
+<li>Ваш город - <span>Москва</span></li>
+<li><a href="o-nas.php">О нас</a></li>
+<li><a href="magaziny.php">Магазины</a></li>
+<li><a href="contacts.php">Контакты</a></li>
+</ul>
+
+<p id="reg-auth-title" align="right"><a class="top-auth">Вход</a><a href="registration.php">Регистрация</a></p>
+
+</div>
+<div id="top-line"></div>
+<img id="img-logo" src="/images/logo.png"/>
+
+<div id="personal-info">
+<p style="margin: 5px; margin-right: 45px;">Звонок бесплатный.</p>
+<h3 align="right">8 (800) 100-12-34</h3>
+<img src="/images/phone-icon.png"/>
+<p align="right">Режим работы:</p>
+<p align="right">Будние дни: с 9:00 до 18:00.</p>
+<p align="right">Суббота, Воскресенье - выходные</p>
+<img src="/images/time-icon.png"/>
+</div>
+ 
+</div>
---
+++ www/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=windows-1251"/>
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+    <link href="css/reset.css" rel="stylesheet" type="text/css" />
+	<title>Интернет-Магазин Цифровой Техники</title>
+</head>
+
+<body>
+<div id="block-body">
+
+<?php
+    include("include/block-header.php");
+?>
+
+<div id="block-right"></div>
+<div id="block-content"></div>
+
+<?php
+    include("include/block-footer.php");
+?>
+</div>
+
+</body>
+</html>
