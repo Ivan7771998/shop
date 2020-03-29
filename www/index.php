ae28c12acde90178624f8df10890983ed2fa5377
Ivan <Ivan7771998>
2020-03-29 19:47:37 +0300

    finish lessons 9

--- www/index.php
+++ www/index.php
@@ -4,6 +4,10 @@
 	<meta http-equiv="content-type" content="text/html; charset=windows-1251"/>
     <link href="css/style.css" rel="stylesheet" type="text/css" />
     <link href="css/reset.css" rel="stylesheet" type="text/css" />
+    <script type="text/javascript" src="/js/jquery-1.8.2.min.js"></script>
+    <script type="text/javascript" src="/js/jcarousellite_1.0.1.js"></script>
+    <script type="text/javascript" src="/js/shop-script.js"></script>
+    <script type="text/javascript" src="/js/jquery.cookie.min.js"></script>
 	<title>Интернет-Магазин Цифровой Техники</title>
 </head>
 
@@ -14,7 +18,13 @@
     include("include/block-header.php");
 ?>
 
-<div id="block-right"></div>
+<div id="block-right">
+<?php
+    include("include/block-category.php");
+    include("include/block-parameter.php");
+    include("include/block-news.php");
+?>
+</div>
 <div id="block-content"></div>
 
 <?php
